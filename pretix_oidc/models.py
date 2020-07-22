4ea5879e235d2e36325a77315cbb2aac99ed7f9b
Jaakko Rinta-Filppula <jaakko@r-f.fi>
2020-07-23 01:13:39 +0300

    Add OIDCTeamAssignmentRule model

---
+++ pretix_oidc/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from pretix.base.models import Team
+
+
+class OIDCTeamAssignmentRule(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    attribute = models.CharField(max_length=255, verbose_name=_('OIDC attribute'))
+    value = models.CharField(max_length=255, verbose_name=_('Attribute value'))
+
+    class Meta:
+        verbose_name = _('Team assignment rule')
